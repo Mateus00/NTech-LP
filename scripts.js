e67365d7d170440399a4cd3051ff28dc168e799b
Mateus <mateusoliveiranf@gmail.com>
2025-11-02 15:06:31 -0300

    Seção de pacotes e animção fade-in

--- scripts.js
+++ scripts.js
@@ -8,3 +8,26 @@ function moveCarousel() {
 }
 
 setInterval(moveCarousel, 5000); // muda a cada 4 segundos
+
+document.addEventListener("DOMContentLoaded", () => {
+    const faders = document.querySelectorAll(".fade-in");
+
+    const appearOptions = {
+      threshold: 0.1, // 20% do elemento visível ativa o efeito
+      rootMargin: "0px 0px -50px 0px"
+    };
+
+    const appearOnScroll = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+      appearOnScroll.observe(fader);
+    });
+  });
+
+
